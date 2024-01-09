dc023f85c0553c1bbfbb3e04fed2aae3e9a2605a
bob <y.smirnov01@gmail.com>
2024-01-09 16:39:49 +0700

    база данных

--- src/main/java/com/example/servingwebcontent/controllers/AccountController.java
+++ src/main/java/com/example/servingwebcontent/controllers/AccountController.java
@@ -1,5 +1,8 @@
 package com.example.servingwebcontent.controllers;
 
+import com.example.servingwebcontent.models.UserData;
+import com.example.servingwebcontent.repo.UserDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
+
+    @Autowired
+    private UserDataRepository userDataRepos;
+
     @GetMapping("/account")
     public String getMethodName(Model model) {
+        Iterable<UserData> userData = userDataRepos.findAll();
+        model.addAttribute("users", userData);
         return "accountMain";
     }
     
